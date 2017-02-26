8cf3018d652efa033511fae21206ec565063aa1f
Tomek Bujakowski <bujakowski@gmail.com>
2017-02-26 09:25:53 +0100

    Add epileptic background in start

--- server/static/js.js
+++ server/static/js.js
@@ -1,1 +1,20 @@
+(function() {
+    var s = document.getElementById('wrap').style,
+        f = false,
+        c1 = 'steelblue',
+        c2 = 'orangered',
+        c3 = 'crimson';
 
+
+    setInterval(function() {
+        s.background = f ? c1 : c2;
+        s.background = f ? c2 : c3;
+        s.background = f ? c3 : c1;
+        s.color = f ? c2 : c1;
+        f = !f;
+    }, 600);
+
+    setInterval(function () {
+        s.background = f ? c2 : c3;
+    }, 600)
+})();
